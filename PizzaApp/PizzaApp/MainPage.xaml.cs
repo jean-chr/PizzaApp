0b4e288fbba52eddd455781a072a75186ce443f8
Jean-Christ BODJONA <jeanchristbodjona@gmail.com>
2022-11-16 07:55:27 +0000

    Mercredi 16 nov

---
+++ PizzaApp/PizzaApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿
+using PizzaApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PizzaApp
+{
+    public partial class MainPage : ContentPage
+    {
+            List<Pizza> pizzalist;
+        public MainPage()
+        {
+            InitializeComponent();
+            pizzalist = new List<Pizza>();
+            
+            pizzalist.Add(new Pizza { Nom = "carnivore", Prix = 5000, Ingredient = new string[]{ "tomato" } });
+            pizzalist.Add(new Pizza { Nom = "vegetarienne", Prix = 7000, Ingredient = new string[]{ "Tomote", "oignon"} });
+            pizzalist.Add(new Pizza { Nom = "Montagnarde", Prix = 2500, Ingredient = new string[] { "Tomote", "oignon" ,"Fromage","poivron"} });
+            
+            
+            //on definit la source de la liste view
+           maliste.ItemsSource = pizzalist;
+        }
+
+        private void Label_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PizzaApp/PizzaApp/Models/Pizza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaApp.Models
+{
+    public class Pizza
+    {
+        public string Nom { get; set; }
+        public int Prix { get; set; }
+        public string[] Ingredient { get; set; }
+        public string Prixdevice { get { return Prix + " FCFA"; } }
+        public string ListIngredient { get { return String.Join( " ", Ingredient); } }
+
+        public Pizza() { }
+    }
+
+ 
+
+}
